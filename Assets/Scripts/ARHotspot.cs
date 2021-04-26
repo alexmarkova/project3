eabc94213d860ea33ea1be2aacc22146fe1b0c0f
alexmarkova <35998157+alexmarkova@users.noreply.github.com>
2021-04-26 23:46:11 +0400

    layout updates, particles, fx

---
+++ Assets/Scripts/ARHotspot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARHotspot : MonoBehaviour
+{
+
+    private Transform mainCamera;
+    private Transform arSessionOrigin;
+
+    void Start()
+    {
+        mainCamera = Camera.main.transform;
+        arSessionOrigin = mainCamera.parent;
+    }
+
+    public void OnTap()
+    {
+        float distance = Vector3.Distance(transform.position, mainCamera.position);
+        Vector3 destination = transform.position - mainCamera.localPosition;
+
+        LeanTween.move(arSessionOrigin.gameObject, destination + Vector3.up, distance / 2f);
+    }
+
+
+}
