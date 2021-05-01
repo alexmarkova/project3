d88e449c202052c944286bb831aeb4f35d741dce
callmeswee <47381283+callmeswee@users.noreply.github.com>
2021-05-01 21:47:53 +0400

    SoundIteration_1
    sound is being worked on, new scene for sound work

---
+++ Assets/3_sreeScripts/collidingSoundScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collidingSoundScript : MonoBehaviour
+{
+
+    public AudioSource bounceSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bounceSound = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        bounceSound.Play();
+    }
+}
