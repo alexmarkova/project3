0d148b6c009927f1d490acc4aa61e5e516de0740
callmeswee <47381283+callmeswee@users.noreply.github.com>
2021-05-03 14:53:38 +0400

    glitchy portal
    lol

---
+++ Assets/3_sreeScripts/DarktToHappy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DarktToHappy : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "MainCamera")
+        {
+            SceneManager.LoadScene(0);
+        }
+
+
+
+    }
+}
